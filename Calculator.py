a57e8edad9e09316ea23dae93bd11765e21d121b
pphariiii <pphariiii@gmail.com>
2026-01-01 22:01:46 +0700

    Add simple calculator program

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+print("⏔⏔⏔ ꒰ ᧔ Calculator ᧓ ꒱ ⏔⏔⏔")
+n1 = float(input("Insert a number : "))
+sym = input("Choose a symbol (+, -, *, /) : ")
+n2 = float(input("Insert a number : "))
+if sym == "+" :
+  result = n1+n2
+elif sym == "-" :
+  result = n1-n2
+elif sym == "*" :
+  result = n1*n2
+elif sym == "/" :
+  if n2 !=0 :
+  else:
+    reseult = "Can't divided by 0"
+else:
+  result = "Invalid symbol !!"
+
+print("Result : ", result)
+print("· · ─ ·ʚɞ· ─ · ·")
